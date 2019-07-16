2050f4cd40595c44b9a6223cb02b7212deb9a08c
xus <1010665027@qq.com>
2019-07-16 22:24:58 +0800

    idea整合git测试

---
+++ src/main/java/com/my/springboot/SpringbootApplication.java
@@ -0,0 +1,22 @@
+package com.my.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @RequestMapping("/show")
+    public String show(){
+        return "hello,idea!";
+    }
+}
